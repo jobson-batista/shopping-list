c07103371fcb5f458dbf4be2dbec558a4cdfc5cd
Dev Jobson <jobson2084@gmail.com>
2026-03-06 13:31:32 -0300

    Spring Boot Project created

---
+++ backend/src/test/java/com/biglist/api/ShoppingListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biglist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
