1f608590c7d6472129d4dd0ed541a96d99a36a87
Dev Jobson <jobson2084@gmail.com>
2026-03-06 17:42:02 -0300

    Organizing project

--- backend/src/main/java/com/shoppinglist/api/ShoppingListApiApplication.java
+++ backend/src/main/java/com/shoppinglist/api/ShoppingListApiApplication.java
@@ -1,4 +1,4 @@
-package com.biglist.api;
+package com.shoppinglist.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/shoppinglist/api/ShoppingListApiApplicationTests.java
+++ backend/src/test/java/com/shoppinglist/api/ShoppingListApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.biglist.api;
+package com.shoppinglist.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
